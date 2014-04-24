4d09be5e6d15680aafb6e2cfd9c828acaf6ed840
jarod68 <milou68@gmail.com>
2014-04-24 22:55:22 +0200

    Implement all charts

--- script.js
+++ script.js
@@ -37,7 +37,6 @@
 
 	}
 
-
 	function loadMean(from, to){
 
 		var result = null;
@@ -47,11 +46,11 @@
 			'url': "./mean.php?from=" + computeDateForParameter(from)+ "&to="+computeDateForParameter(to),
 			'dataType': "json",
 			'success': function (json) {
-
 				var arr = [];
 				var lim = json.length;
 				for (var i = 0; i < lim; i++){
 					arr.push({day : toDate(json[i].day), indoor_temp_mean: json[i].indoor_temp_mean, indoor_humidity_mean: json[i].indoor_humidity_mean, indoor_pressure_mean: json[i].indoor_pressure_mean, outdoor_temperature_mean: json[i].outdoor_temperature_mean });
+					
 				}
 				result = arr;
 			}
@@ -221,7 +220,7 @@
 		$("#meanMonthTemp").dxChart({
 
 			dataSource: data,
-			type: 'bar',
+			type: 'stackedBar',
 			commonSeriesSettings: {
 				argumentField: 'day'
 			},
@@ -232,6 +231,43 @@
 			series: [{
 				name: 'outdoor_temperature_mean',
 				valueField: 'outdoor_temperature_mean',
+				type:'stackedBar'
+			}], 
+			argumentAxis: {
+				argumentType: 'datetime',
+				label: {
+					format: 'dd/MM/yy'
+				},
+				axisDivisionFactor: 50,
+				grid: {
+					visible: true
+				}
+			},  tooltip: {
+				enabled: true,
+				customizeText: function (label) {
+					var timestamp = new Date(this.argumentText);
+					return  this.valueText + '&#176C, ' + timestamp.getUTCDate() + "/" + (timestamp.getUTCMonth()+1) + "/" + timestamp.getUTCFullYear() ;
+				}
+			}
+		});
+	}
+
+	function loadMonthHumChart(data){
+		
+		$("#meanMonthHum").dxChart({
+
+			dataSource: data,
+			type: 'bar',
+			commonSeriesSettings: {
+				argumentField: 'day'
+			},
+			scale: {
+				valueType: "datetime"
+			},
+
+			series: [{
+				name: 'indoor_humidity_mean',
+				valueField: 'indoor_humidity_mean',
 				type:'bar'
 			}], 
 			argumentAxis: {
@@ -253,6 +289,42 @@
 		});
 	}
 
+	function loadMonthPressChart(data){
+		
+		$("#meanMonthPress").dxChart({
+
+			dataSource: data,
+			type: 'bar',
+			commonSeriesSettings: {
+				argumentField: 'day'
+			},
+			scale: {
+				valueType: "datetime"
+			},
+
+			series: [{
+				name: 'indoor_pressure_mean',
+				valueField: 'indoor_pressure_mean',
+				type:'bar'
+			}], 
+			argumentAxis: {
+				argumentType: 'datetime',
+				label: {
+					format: 'dd/MM/yy'
+				},
+				axisDivisionFactor: 50,
+				grid: {
+					visible: true
+				}
+			},  tooltip: {
+				enabled: true,
+				customizeText: function (label) {
+					var timestamp = new Date(this.argumentText);
+					return  this.valueText + '&#176C, ' + timestamp.getUTCDate() + "/" + (timestamp.getUTCMonth()+1) + "/" + timestamp.getUTCFullYear() ;
+				}
+			}
+		});
+	}
 	$(function () {
 		var today = loadDay(new Date());
 
@@ -262,11 +334,10 @@
 
 		var oneMonthAgo = new Date();
 		oneMonthAgo.setDate(oneMonthAgo.getDate() - 31);
-		console.log(oneMonthAgo);
 		var month = loadMean(oneMonthAgo, new Date());
-		console.log(month);
 		loadMonthTempChart(month);
-
+		loadMonthHumChart(month);
+		loadMonthPressChart(month);
 	});
 
 /*
