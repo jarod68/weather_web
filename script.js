a40a0adc8d1a329388379886f47f8530e90a3d95
jarod68 <milou68@gmail.com>
2014-04-24 23:05:45 +0200

    Change date duplication and remove dead commented code

--- script.js
+++ script.js
@@ -50,7 +50,7 @@
 				var lim = json.length;
 				for (var i = 0; i < lim; i++){
 					arr.push({day : toDate(json[i].day), indoor_temp_mean: json[i].indoor_temp_mean, indoor_humidity_mean: json[i].indoor_humidity_mean, indoor_pressure_mean: json[i].indoor_pressure_mean, outdoor_temperature_mean: json[i].outdoor_temperature_mean });
-					
+
 				}
 				result = arr;
 			}
@@ -58,16 +58,6 @@
 		return result;
 
 	}
-
-	//var today = loadDay(new Date());
-
-	//var yesterday = new Date();
-	//yesterday.setDate(yesterday.getDate() - 31);
-
-	//var month = loadMean(yesterday, new Date());
-	// "timestamp":"2014-04-01 06:00:03","indoor_temp":23,"indoor_humidity":59,"indoor_pressure":999.8,"
-
-
 
 	function loadTempChart(data){
 
@@ -339,92 +329,3 @@
 		loadMonthHumChart(month);
 		loadMonthPressChart(month);
 	});
-
-/*
-	function loadChart(date){
-
-		
-	}*/
-
-
-/*
-	$(function () {
-		$("#dayHum").dxChart({
-
-			dataSource: today,
-			type: 'stackedLine',
-			commonSeriesSettings: {
-				argumentField: 'timestamp'
-			},
-			scale: {
-				valueType: "datetime"
-			},
-			series: [{
-				name: 'indoor_humidity',
-				valueField: 'indoor_humidity', 
-				color: 'violet', 
-				point: {
-					size: '0.5'
-				}
-			}], valueAxis:{
-				label: {
-					customizeText: function () {
-						return this.valueText + '%';
-					}
-				}
-			},
-			argumentAxis: {
-				argumentType: 'datetime',
-				label: {
-					format: 'HH:mm'
-				},
-				axisDivisionFactor: 50,
-				grid: {
-					visible: true
-				}
-			},  tooltip: {
-				enabled: true,
-				customizeText: function (label) {
-
-					return  this.valueText + '%, ' + computeDateForTooltips(new Date(this.argumentText)) ;
-				}
-			}
-
-		});
-});*/
-/*
-	$(function () {
-		$("#meanTemp").dxChart({
-
-			dataSource: month,
-			type: 'bar',
-			commonSeriesSettings: {
-				argumentField: 'day'
-			},
-			scale: {
-				valueType: "datetime"
-			},
-
-			series: [{
-				name: 'outdoor_temperature_mean',
-				valueField: 'outdoor_temperature_mean',
-				type:'bar'
-			}], 
-			argumentAxis: {
-				argumentType: 'datetime',
-				label: {
-					format: 'dd/MM/yy'
-				},
-				axisDivisionFactor: 50,
-				grid: {
-					visible: true
-				}
-			},  tooltip: {
-				enabled: true,
-				customizeText: function (label) {
-					var timestamp = new Date(this.argumentText);
-					return  this.valueText + '&#176C, ' + timestamp.getUTCDate() + "/" + (timestamp.getUTCMonth()+1) + "/" + timestamp.getUTCFullYear() ;
-				}
-			}
-		});
-	});*/
