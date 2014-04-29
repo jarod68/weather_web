58c6889291b924512c3903fb7ed2c9ba480a823c
jarod68 <milou68@gmail.com>
2014-04-29 22:03:48 +0200

    Change div in index

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
 <body>
 
   <?php
-  include_once "toolbar.php";
-  toolbar::getInstance()->write();
+    include_once "toolbar.php";
+    toolbar::getInstance()->write();
   ?>
   
 
   <div id="core" class="container">
-    <div class="row" id="gaugeContainer">
-      <h3>Gauges</h3>
-      <div id="gaugeOutTemp" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
-      <div id="gaugeInTemp" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
-      <div id="gaugeHum" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
-      <div id="gaugePress" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
+      <div class="row" id="gaugeContainer">
+        <h3>Gauges</h3>
+        <div id="gaugeOutTemp" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
+        <div id="gaugeInTemp" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
+        <div id="gaugeHum" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
+        <div id="gaugePress" style="max-width:400px;height: 300px;margin:20px;" class="col-md-8 jumbotron"></div>
+      </div>
     </div>
-  </div>
-</body>
-</html>
+  </body>
+  </html>
